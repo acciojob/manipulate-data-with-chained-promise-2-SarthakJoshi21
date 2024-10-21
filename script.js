90923e738016720d618cb1a71720c4f21869f87d
SarthakJoshi21 <157214236+SarthakJoshi21@users.noreply.github.com>
2024-10-21 20:40:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+ function getData() {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve([1, 2, 3, 4]);
+        }, 3000);
+      });
+    }
+
+    function filterEven(numbers) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const evenNumbers = numbers.filter(num => num % 2 === 0);
+          resolve(evenNumbers);
+        }, 1000);
+      });
+    }
+
+    function multiplyByTwo(numbers) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const multipliedNumbers = numbers.map(num => num * 2);
+          resolve(multipliedNumbers);
+        }, 2000);
+      });
+    }
+
+    getData()
+      .then(filterEven)
+      .then(multiplyByTwo)
+      .then(result => {
+        const outputDiv = document.getElementById("output");
+        outputDiv.textContent = result.join(", ");
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
